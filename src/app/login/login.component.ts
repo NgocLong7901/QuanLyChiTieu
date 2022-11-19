7f6c4b94ad9d3691aa0cfcaae4b5938f325e21fa
Ngoc Long <ngoclong792001@gmail.com>
2022-11-19 20:38:49 +0700

    commit lan 3 19/11/2022

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,40 +15,36 @@ export class LoginComponent implements OnInit {
   loginSuccess = false;
   username = "";
   id: any;
-  // wallet;
+  wallet: any;
   showMe = false;
   loginForm: any = this.fb.group({
     username: [''],
     password: ['']
   });
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService, private dataService: DataService) { }
 
   ngOnInit(): void {
   }
-  // login() {
-  //   this.authService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe((response) => {
-  //     var code = response.status;
-  //     console.log("status code:" + code);
-  //     if (code == 200) {
-  //       this.invalidLogin = false;
-  //       this.loginSuccess = true;
-  //       this.authService.username = this.loginForm.value.username;
-  //       this.authService.password = this.loginForm.value.password;
-  //       this.authService.registerSuccessfulLogin(this.loginForm.value.username);
-  //       localStorage.setItem('userId', response.body['id_user']);
-  //       localStorage.setItem('user', JSON.stringify(response.body)); // nếu cần lấy toàn bộ thông tin user         
-  //       this.router.navigate(['/index']);
-  //     }
-  //   }, () => {
-  //     this.invalidLogin = true;
-  //     this.loginSuccess = false;
-  //   });
-  // }
+  login() {
+    this.authService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe((response) => {
+      var code = response.status;
+      console.log("status code:" + code);
+      if (code == 200) {
+        this.invalidLogin = false;
+        this.loginSuccess = true;
+        this.authService.username = this.loginForm.value.username;
+        this.authService.password = this.loginForm.value.password;
+        this.authService.registerSuccessfulLogin(this.loginForm.value.username);
+        // localStorage.setItem('userId', response.body['id_user']);
+        localStorage.setItem('user', JSON.stringify(response.body)); // nếu cần lấy toàn bộ thông tin user         
+        this.router.navigate(['/index']);
+      }
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });
+  }
   tabChange() {
-    // this.id=ids;
-    // this.router.navigate(['/'+this.id])
-
     console.log(this.showMe);
-
   }
 }
