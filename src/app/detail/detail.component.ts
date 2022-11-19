c3e322257942c8e8ee4b527f7532c58eb33f3ecc
Ngoc Long <ngoclong792001@gmail.com>
2022-11-19 16:05:15 +0700

    commit 19/11/2022

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
-
+  // username = this.authService.getLoggedInUserName();
+  details: any;
+  public CURRENCY: string | undefined;
+  showMe: boolean = true;
+  sumIn: any;
+  sumSpend: any;
+  selectedDetail: any;
+  message: any;
+  showCategory: boolean | undefined;
+  newForm = this.fb.group({
+    detail: [''],
+    date: [''],
+    id_category: [''],
+    price: [''],
+    note: [''],
+    // username: [this.authService.getLoggedInUserName()],
+    username: ['']
+  });
+  constructor(private fb: FormBuilder) { }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  // ngOnInit(): void {
+  //   this.displayListDetail();
+  //   this.CURRENCY = 'CURRENCY';
+  // }
+  // displayListDetail() {
+  //   this.dataService.getListDetail(this.username).subscribe((data: Array<Detail>) => {
+  //     this.details = data
+  //   })
+  // }
+  // delete(detail: any) {
+  //   this.dataService.deleteDetail(detail.id_detail).subscribe((res) => {
+  //     this.displayListDetail();
+  //   });
+  // }
+  // clickbtnFix(detail: any) {
+  //   this.showMe = false;
+  //   this.dataService.getDetail(detail.id_detail).subscribe((res) => {
+  //     this.selectedDetail = res;
+  //     this.newForm.patchValue({ detail: res.detail });
+  //     if (res.status == 0) {
+  //       this.showCategory = true;
+  //     } else {
+  //       this.showCategory = false;
+  //     }
+  //   })
+  // }
+  // onChangeReactiveForm(key: string) {
+  //   this.newForm.get(key).patchValue(this.newForm.get(key).value);
+  // }
+  // updateDetail() {
+  //   let code;
+  //   const userId = localStorage.getItem('userId');
+  //   let detail = { id: this.selectedDetail.id_detail, detail: "", date: "", id_category: "", price: "", note: "", status: this.selectedDetail.status, username: "", id_user: userId };
+  //   detail.detail = this.newForm.value.detail;
+  //   detail.date = this.newForm.value.date;
+  //   detail.id_category = this.newForm.value.id_category;
+  //   detail.price = this.newForm.value.price;
+  //   detail.note = this.newForm.value.note;
+  //   detail.username = this.newForm.value.username;
+  //   console.log(detail);
+  //   this.dataService.updateDetail(detail).subscribe(
+  //     (response) => { },
+  //     error => {
+  //       code = error.status;
+  //       console.log("status code:" + code);
+  //       if (code == 303)
+  //         this.message = "Bạn cập nhật thành công"
+  //     }
+  //   );
+  // }
+  clickbtnClose() {
+    this.showMe = !this.showMe
+  }
 }
--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -1,15 +1,384 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.component.html',
   styleUrls: ['./statistic.component.css']
 })
 export class StatisticComponent implements OnInit {
-
-  constructor() { }
-
+  formDate: any = this.fb.group({
+    startDate: [''],
+    endDate: [''],
+    year: ['']
+  });
+  constructor(private fb: FormBuilder) { }
+  sumIn: any;
+  sumSpend: any;
+  sumInYear: any;
+  sumSpendYear: any;
+  showMe: boolean | undefined;
+  details: any;
+  dataSpendchart: any;
+  dataIncomechart: any;
+  chart: any;
+  chartIn: any;
+  dataInchartBar = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  chartBar: any;
+  detailsInYear: any;
+  year1: any;
+  selected: number | undefined;
   ngOnInit(): void {
+    this.year1 = this.selectyear();
   }
+  selectyear() {
+    let syear = 2018;
+    var year = new Array();
+    for (var i = 0; i < 16; i++) {
+      year[i] = syear;
+      syear++;
+    }
+    return year;
+  }
+  selectChangeHandler(event: any) {
+    this.selected = event.target.value;
+  }
+  getDetailbyDate() {
+    let startDate = new Date(this.formDate.value.startDate);
+    var sdate = new Intl.DateTimeFormat("ja-JP").format(startDate);
+    let endDate = new Date(this.formDate.value.endDate);
+    var edate = new Intl.DateTimeFormat("ja-JP").format(endDate);
+    // let username = this.authService.getLoggedInUserName();
 
+    //details
+    // this.dataService.getDetailbyDate(username, sdate, edate).subscribe((data: Array<Detail>) => {
+    //   this.details = data;
+    //   var sum = 0;
+    //   var sumSpend = 0
+    //   for (var i = 0; i < data.length; i++) {
+    //     if (data[i].status == 0) {
+    //       sum += data[i].price;
+    //     }
+
+    //     else {
+    //       sumSpend += data[i].price;
+    //     }
+    //   }
+    //   this.sumIn = sum;
+    //   this.sumSpend = sumSpend;
+    // });
+    //data chart spend
+
+    //draw chart pie
+  //   this.dataService.getDataSpendChart(username, sdate, edate).subscribe((data: number[]) => {
+  //     this.dataSpendchart = data;
+
+  //     var dataSpendPie = new Array(0, 0, 0, 0, 0, 0, 0);
+  //     for (var i = 0; i < data.length; i++) {
+  //       switch (data[i][1]) {
+  //         case 1:
+  //           dataSpendPie[0] = data[i][0];
+  //           break;
+  //         case 2:
+  //           dataSpendPie[1] = data[i][0];
+  //           break;
+  //         case 3:
+  //           dataSpendPie[2] = data[i][0];
+  //           break;
+  //         case 4:
+  //           dataSpendPie[3] = data[i][0];
+  //           break;
+  //         case 5:
+  //           dataSpendPie[4] = data[i][0];
+  //           break;
+  //         case 6:
+  //           dataSpendPie[5] = data[i][0];
+  //           break;
+  //         case 7:
+  //           dataSpendPie[6] = data[i][0];
+  //           break;
+  //       }
+  //       console.log(dataSpendPie);
+  //     }
+  //     if (this.chart == null) {
+  //       this.chart = new Chart('canvas', {
+  //         type: 'pie',
+  //         data: {
+  //           labels: [['Xăng dầu'], ['Giải trí'], ['Điện nước'], ['Du lịch'], ['Sức khỏe'], ['Giáo dục'], 'Shopping'],
+  //           datasets: [{
+  //             data: dataSpendPie,
+  //             backgroundColor: [
+  //               '#fe6a6a',
+  //               '#fc4242',
+  //               '#ff1919',
+  //               '#e40000',
+  //               '#c10000',
+  //               '#a70202',
+  //               '#910101',
+  //               '#964564'
+  //             ],
+  //           }]
+  //         },
+  //         options: {
+  //           responsive: true
+  //         }
+  //       })
+  //     }
+  //     else {
+  //       this.chart.destroy();
+  //       this.chart = new Chart('canvas', {
+  //         type: 'pie',
+  //         data: {
+  //           labels: [['Xăng dầu'], ['Giải trí'], ['Điện nước'], ['Du lịch'], ['Sức khỏe'], ['Giáo dục'], 'Shopping'],
+  //           datasets: [{
+  //             data: dataSpendPie,
+  //             backgroundColor: [
+  //               '#fe6a6a',
+  //               '#fc4242',
+  //               '#ff1919',
+  //               '#e40000',
+  //               '#c10000',
+  //               '#a70202',
+  //               '#910101'
+  //             ],
+  //           }]
+  //         },
+  //         options: {
+  //           responsive: true
+  //         }
+  //       })
+  //     }
+
+  //   });
+  //   //data chart income
+  //   this.dataService.getDataIncomeChart(username, sdate, edate).subscribe((data: number[]) => {
+  //     this.dataIncomechart = data;
+  //     var dataInPie = new Array(0, 0, 0, 0);
+  //     for (var i = 0; i < data.length; i++) {
+  //       switch (data[i][1]) {
+
+  //         case 10:
+  //           dataInPie[0] = data[i][0];
+  //           break;
+  //         case 11:
+  //           dataInPie[1] = data[i][0];
+  //           break;
+  //         case 12:
+  //           dataInPie[2] = data[i][0];
+  //           break;
+  //         case 13:
+  //           dataInPie[3] = data[i][0];
+  //           break;
+  //       }
+  //     }
+  //     if (this.chartIn == null) {
+  //       this.chartIn = new Chart('canvasIn', {
+  //         type: 'pie',
+  //         data: {
+  //           labels: [['Lương'], ['Thưởng'], ['Được tặng'], 'Bản đồ'],
+  //           datasets: [{
+  //             data: dataInPie,
+  //             backgroundColor: [
+  //               '#0261ff',
+  //               '#0050d4',
+  //               '#013892',
+  //               '#002665',
+  //               '#5c99ff'
+  //             ],
+  //           }]
+  //         },
+  //         options: {
+  //           responsive: true
+  //         }
+  //       })
+  //     }
+  //     else {
+  //       this.chartIn.destroy();
+  //       this.chartIn = new Chart('canvasIn', {
+  //         type: 'pie',
+  //         data: {
+  //           labels: [['Lương'], ['Thưởng'], ['Được tặng'], 'Bản đồ'],
+  //           datasets: [{
+  //             data: dataInPie,
+  //             backgroundColor: [
+  //               '#0261ff',
+  //               '#0050d4',
+  //               '#013892',
+  //               '#002665',
+  //               '#5c99ff'
+  //             ],
+  //           }]
+  //         },
+  //         options: {
+  //           responsive: true
+  //         }
+  //       })
+  //     }
+  //   });
+  // }
+  // getChartBar() {
+  //   let username = this.authService.getLoggedInUserName();
+  //   var dataInBar = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  //   var dataSpendBar = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  //   //get detail
+  //   this.dataService.getDetailbyYear(username, this.selected).subscribe((data: Array<Detail>) => {
+  //     this.detailsInYear = data;
+  //     var sum = 0;
+  //     var sumSpend = 0
+  //     for (var i = 0; i < data.length; i++) {
+  //       if (data[i].status == 0) {
+  //         sum += data[i].price;
+  //       }
+
+  //       else {
+  //         sumSpend += data[i].price;
+  //       }
+  //     }
+  //     this.sumInYear = sum;
+  //     this.sumSpendYear = sumSpend;
+  //   });
+  //   //data chart Bar
+  //   this.dataService.getDataChartBar(username, this.selected).subscribe((data: number[]) => {
+  //     this.dataInchartBar = data;
+  //     for (var i = 0; i < data.length; i++) {
+  //       if (data[i][2] == 0) {
+  //         switch (data[i][1]) {
+  //           case 1:
+  //             dataInBar[0] = data[i][0];
+  //             break;
+  //           case 2:
+  //             dataInBar[1] = data[i][0];
+  //             break;
+  //           case 3:
+  //             dataInBar[2] = data[i][0];
+  //             break;
+  //           case 4:
+  //             dataInBar[3] = data[i][0];
+  //             break;
+  //           case 5:
+  //             dataInBar[4] = data[i][0];
+  //             break;
+  //           case 6:
+  //             dataInBar[5] = data[i][0];
+  //             break;
+  //           case 7:
+  //             dataInBar[6] = data[i][0];
+  //             break;
+  //           case 8:
+  //             dataInBar[7] = data[i][0];
+  //             break;;
+  //           case 9:
+  //             dataInBar[8] = data[i][0];
+  //             break;
+  //           case 10:
+  //             dataInBar[9] = data[i][0];
+  //             break;
+  //           case 11:
+  //             dataInBar[10] = data[i][0];
+  //             break;
+  //           case 12:
+  //             dataInBar[11] = data[i][0];
+  //             break;
+  //         }
+  //       } else {
+  //         switch (data[i][1]) {
+  //           case 1:
+  //             dataSpendBar[0] = data[i][0];
+  //             break;
+  //           case 2:
+  //             dataSpendBar[1] = data[i][0];
+  //             break;
+  //           case 3:
+  //             dataSpendBar[2] = data[i][0];
+  //             break;
+  //           case 4:
+  //             dataSpendBar[3] = data[i][0];
+  //             break;
+  //           case 5:
+  //             dataSpendBar[4] = data[i][0];
+  //             break;
+  //           case 6:
+  //             dataSpendBar[5] = data[i][0];
+  //             break;
+  //           case 7:
+  //             dataSpendBar[6] = data[i][0];
+  //             break;
+  //           case 8:
+  //             dataSpendBar[7] = data[i][0];
+  //             break;;
+  //           case 9:
+  //             dataSpendBar[8] = data[i][0];
+  //             break;
+  //           case 10:
+  //             dataSpendBar[9] = data[i][0];
+  //             break;
+  //           case 11:
+  //             dataSpendBar[10] = data[i][0];
+  //             break;
+  //           case 12:
+  //             dataSpendBar[11] = data[i][0];
+  //             break;
+  //         }
+  //       }
+  //       console.log(this.dataInchartBar[i][2]);
+  //     }
+  //     console.log(dataInBar);
+  //     console.log(dataSpendBar);
+
+  //     //draw chart
+  //     if (this.chartBar == null) {
+  //       this.chartBar = new Chart('chartBar', {
+  //         type: 'bar',
+  //         data: {
+  //           labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+  //           datasets: [
+  //             { data: dataSpendBar, label: 'Chi tiêu' },
+  //             { data: dataInBar, label: 'Thu Nhập' }
+
+  //           ]
+  //         },
+  //         options: {
+  //           responsive: true,
+  //           scales: {
+  //             x: {},
+  //             y: {
+  //               min: 10
+  //             }
+  //           },
+  //           plugins: {
+  //             legend: {
+  //               display: true,
+  //             }
+  //           }
+  //         }
+  //       })
+  //     } else {
+  //       this.chartBar.destroy();
+  //       this.chartBar = new Chart('chartBar', {
+  //         type: 'bar',
+  //         data: {
+  //           labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+  //           datasets: [
+  //             { data: dataSpendBar, label: 'Chi tiêu' },
+  //             { data: dataInBar, label: 'Thu nhập ' }
+  //           ]
+  //         },
+  //         options: {
+  //           responsive: true,
+  //           scales: {
+  //             x: {},
+  //             y: {
+  //               min: 10
+  //             }
+  //           },
+  //           plugins: {
+  //             legend: {
+  //               display: true,
+  //             }
+  //           }
+  //         }
+  //       })
+  //     }
+
+  //   });
+  }
 }
